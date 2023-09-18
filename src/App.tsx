80e6740e346974c234a23ffc95ba9152b272af9a
Flavio Pinto <flaviopnt@hotmail.it>
2023-09-18 19:53:21 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import 'normalize.css'
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>New York Times</h1>
+    </>
+  )
+}
+
+export default App
