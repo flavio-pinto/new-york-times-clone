2b2cc06b825b648532d837a4f5ec496cd635ea5c
Flavio Pinto <flaviopnt@hotmail.it>
2023-09-23 17:23:51 +0200

    [FEAT] Header responsiveness

---
+++ src/components/DropDownMenu/DropDownMenu.tsx
@@ -0,0 +1,16 @@
+
+import styles from './DropDownMenu.module.css'
+
+/* type Props = {} */
+
+const HamburgerMenu = (/* props: Props */) => {
+  return (
+    <div className={styles.DropDownMenu}>
+      <ul className={styles.DropDownMenuList}>
+        <li>Placeholder</li>
+      </ul>
+    </div>
+  )
+}
+
+export default HamburgerMenu
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,65 @@
-import styles from './Header.module.css'
+import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
+import { FaUser } from "react-icons/fa";
+import DropDownMenu from "../DropDownMenu/DropDownMenu";
+import styles from "./Header.module.css";
+import { useState } from "react";
 /* type Props = {} */
 
 const Header = (/* props: Props */) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleDropDown = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="d-flex justify-content-start align-items-center">
-      <div>Placeholder</div>
-      <h1 className={`${styles.h1}`}>The New York Times</h1>
-      <div>Placeholder</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-2 align-self-center d-flex justify-content-start ps-0">
+            {!isMenuOpen && (
+              <RxHamburgerMenu
+                className="d-lg-none"
+                onClick={() => toggleDropDown()}
+              />
+            )}
+            {isMenuOpen && <DropDownMenu />}
+          </div>
+          <div className="col-8">
+            <h1 className={`${styles.h1}`}>The New York Times</h1>
+          </div>
+          <div className="col-2 align-self-center d-flex justify-content-end pe-0">
+            {isMenuOpen ? (
+              <RxCross1 onClick={() => toggleDropDown()} />
+            ) : (
+              <FaUser className="d-lg-none" />
+            )}
+          </div>
+        </div>
+      </div>
     </header>
   )
+
+  {
+    /* <header className="d-flex justify-content-start align-items-center">
+      <div className={styles.headerLeft}>
+        {!isMenuOpen && (
+          <RxHamburgerMenu className="d-lg-none" onClick={() => toggleDropDown()} />
+        )}
+      </div>
+      {isMenuOpen && (
+        <DropDownMenu />
+      )}
+      <h1 className={`${styles.h1}`}>The New York Times</h1>
+      <div className={`${styles.headerRight} d-`}>
+        {isMenuOpen ? ( 
+          <RxCross1 onClick={() => toggleDropDown()} />
+        ) : (
+          <FaUser />
+        )}
+      </div>
+    </header>*/
+  }
 }
 
-export default Header
+export default Header;
