8ccd2f050ea3105497f30a79f3d11c6409f5212b
Flavio Pinto <flaviopnt@hotmail.it>
2023-10-02 22:57:49 +0200

    [FEAT] Error page and no results output

--- src/Pages/Error/Error.tsx
+++ src/Pages/Error/Error.tsx
@@ -1,8 +1,20 @@
+import styles from "./Error.module.css"
 /* type Props = {} */
+
+import { Col, Container, Row } from "react-bootstrap"
 
 const Error = (/* props: Props */) => {
   return (
-    <div>Error</div>
+    <main className={styles.errorPage}>
+      <Container>
+        <Row>
+          <Col xs={12} md={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }} className="my-5">
+            <h2>Page Not Found</h2>
+            <p>We’re sorry, we seem to have lost this page, but we don’t want to lose you. Report the broken link to our email.</p>
+          </Col>
+        </Row>
+      </Container>
+    </main>
   )
 }
 
--- src/Pages/SearchResultsPage/SearchResultsPage.tsx
+++ src/Pages/SearchResultsPage/SearchResultsPage.tsx
@@ -5,11 +5,12 @@ import { Col, Container, Row } from "react-bootstrap";
 import SingleNewsFromSearch from "../../components/SingleNewsFromSearch/SingleNewsFromSearch";
 import { NewsFromSearch } from "../../interfaces/NewsFromSearch";
 import styles from "./SearchResultsPage.module.css";
+import { RingLoader } from "react-spinners";
 
 /* type Props = {} */
 
 const SearchResultsPage = (/* props: Props */) => {
-  const { query } = useParams();
+  const { query } = useParams()
   const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=${
     import.meta.env.VITE_API_KEY
   }`;
@@ -17,6 +18,24 @@ const SearchResultsPage = (/* props: Props */) => {
 
   console.log(query, url);
   console.log(news);
+
+  /* if(news.length === 0) {
+    navigate('*')
+  } */
+
+  if (news.length === 0) {
+    return (
+      <>
+        <MainDate />
+        <main className={`${styles.mainNewsSection} ps-0`}>
+          <Container>
+            <p className={styles.showingResults}>Showing results for: <span>{query}</span></p>
+            <p className={styles.noResults}>Sorry: no results match your search criteria!</p>
+          </Container>
+        </main>
+      </>
+    );
+  }
 
   if (isDataReady) {
     return (
@@ -37,6 +56,14 @@ const SearchResultsPage = (/* props: Props */) => {
           </Container>
         </main>
       </>
+    );
+  } else {
+    return (
+      <RingLoader
+        className="d-block mx-auto my-5"
+        size={180}
+        aria-label="Loading Spinner"
+      />
     );
   }
 };
