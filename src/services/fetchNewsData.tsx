cc2fdea7ba5f18f93f478edcb1cbf0e3832551b8
Flavio Pinto <flaviopnt@hotmail.it>
2023-10-04 21:53:53 +0200

    [REFACTORING] Removed some console logs and semicolon in Fetch service

--- src/services/fetchNewsData.tsx
+++ src/services/fetchNewsData.tsx
@@ -16,11 +16,9 @@ const useFetchNewsData = (apiUrl: string, isSearch: boolean) => {
     const fetchData = async () => {
       try {
         const response = await axios.get(apiUrl)
-        console.log(response.data)
         setNews(!isSearch ? response.data.results : response.data.response.docs)
         setIsDataReady(true)
       } catch (error) {
-        console.error(error)
         navigate('*')
       }
     }  
