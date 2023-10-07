494459ad542e678f5b523311e9dcaad7ed925708
Flavio Pinto <flaviopnt@hotmail.it>
2023-10-08 00:42:15 +0200

    [FEAT] Error page helmet

--- src/Pages/Error/Error.tsx
+++ src/Pages/Error/Error.tsx
@@ -1,20 +1,34 @@
-import styles from "./Error.module.css"
+import { Helmet } from "react-helmet";
+import styles from "./Error.module.css";
 
-import { Col, Container, Row } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap";
 
 const Error = () => {
   return (
-    <main className={styles.errorPage}>
-      <Container>
-        <Row>
-          <Col xs={12} md={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }} className="my-5">
-            <h2>Page Not Found</h2>
-            <p>We’re sorry, we seem to have lost this page, but we don’t want to lose you. Report the broken link to our email.</p>
-          </Col>
-        </Row>
-      </Container>
-    </main>
-  )
-}
+    <>
+      <Helmet>
+        <title>The New York Times - Error Page</title>
+      </Helmet>
+      <main className={styles.errorPage}>
+        <Container>
+          <Row>
+            <Col
+              xs={12}
+              md={{ span: 8, offset: 2 }}
+              lg={{ span: 6, offset: 3 }}
+              className="my-5"
+            >
+              <h2>Page Not Found</h2>
+              <p>
+                We’re sorry, we seem to have lost this page, but we don’t want
+                to lose you. Report the broken link to our email.
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </main>
+    </>
+  );
+};
 
-export default Error
+export default Error;
